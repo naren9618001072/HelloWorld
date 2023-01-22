71ed10c2863e24afd7bb26dba1dde4b6fd785110
naren9618001072 <panarendra2@gmail.com>
2023-01-22 14:40:58 +0530

    last commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 for(int i=0;i<=20;i++){
 System.out.println("Hello World");
 
-System.out.println('my name is narendr')
+System.out.printl('my name is narendr')
 
 }
 }
